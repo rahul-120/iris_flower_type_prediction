a9caf0a0613a1cbdd2d82821d39c4b42aaf4e422
Rahul <Rahul23232@users.noreply.huggingface.co>
2026-05-06 07:11:18 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+import gradio as gr
+import joblib
+import numpy as np
+
+# Load model
+model = joblib.load("iris_knn_model.pkl")
+
+# Prediction function
+def predict(sepal_length, sepal_width, petal_length, petal_width):
+    data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    prediction = model.predict(data)[0]
+    
+    species = ["setosa", "versicolor", "virginica"]
+    return species[prediction]
+
+# Gradio UI
+interface = gr.Interface(
+    fn=predict,
+    inputs=[
+        gr.Number(label="Sepal Length"),
+        gr.Number(label="Sepal Width"),
+        gr.Number(label="Petal Length"),
+        gr.Number(label="Petal Width")
+    ],
+    outputs="text",
+    title="🌸 Iris Flower Classifier"
+)
+
+interface.launch()
